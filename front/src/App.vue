4571e7f31ac084cc9230869f8ec02f59dca63cf8
bekzat <bekzat.kabdrashev@nu.edu.kz>
2022-11-03 11:03:48 +0600

    First setup for front and back

---
+++ front/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  {% raw %}{{ notes }}
+</template>
+
+<script>
+import axios from 'axios'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  data: () => ({
+    notes: []
+  }),
+  components: {
+    HelloWorld,
+  },
+  mounted () {
+    axios
+      .get('http://127.0.0.1:8000/api/notes/')
+      .then((response) => {
+        this.notes = response.data
+      })
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
