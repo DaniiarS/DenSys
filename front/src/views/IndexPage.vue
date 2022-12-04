7b3da1c9e29f9648f2de42a5b0626ef2248ae920
BatyrM <batyrbek.mukhatbekov@nu.edu.kz>
2022-12-04 14:32:56 +0600

    Main Page

--- front/src/views/IndexPage.vue
+++ front/src/views/IndexPage.vue
@@ -1,11 +1,29 @@
 <template>
-  <div class="flex bg-slate-100 m-auto items-center justify-center w-screen h-screen">
-    <button @click="this.$router.push({path: '/login'})">
-
-      Login
-    </button>
-  </div>
+  <index-navbar />
+  <div
+    class="flex bg-slate-100 m-auto items-center justify-center w-screen h-screen"
+  ></div>
 </template>
 
 <script>
+import IndexNavbar from "@/components/IndexNavbar.vue";
+export default {
+  name: "radio-form",
+  data() {
+    return {};
+  },
+  model: {
+    prop: "input_type",
+    event: "update:input_type",
+  },
+  props: ["buttons", "input_type"],
+  components: {
+    IndexNavbar,
+  },
+  methods: {
+    updateActive(type) {
+      this.$emit("update:input_type", type);
+    },
+  },
+};
 </script>
